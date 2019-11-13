d420523b74ab987d17b14079908bdf12b8eb425d
canhgianguyen <canhgianguyen@gmail.com>
2019-11-13 19:30:40 +0700

    Add new Activity GiaiPTBacHai

---
+++ app/src/main/java/com/example/testkiemtra/GiaiPTBacHaiActivity.java
@@ -0,0 +1,14 @@
+package com.example.testkiemtra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiaiPTBacHaiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giai_ptbac_hai);
+    }
+}
